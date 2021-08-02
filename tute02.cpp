5c3c7c87dbaabfec720ca17883fe7bd871c03f36
it21039836 <88015102+IT21039836@users.noreply.github.com>
2021-08-02 18:03:16 +0000

    I have finished ex,2

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,34 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
-{
-   double salary, netSalary;
+ #include<iostream>
+ #include<iomanip>
+
+ int main(){
+   double salary, netSallary;
    int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
+
+   std::cout<<"Enter Employee Type:";
+   std::cin>>etype;
+   std::cout<<"Enter Salary:";
+   std::cin>>salary;
+   std::cout<<"Enter othrs:";
+   std::cin>>otHrs;
+
+   switch (etype){
+     case 2 : 
+        otRate = 1000;
+        break;
+     case 1 :
+        otRate = 1500;
+        break;
+     default :
+        otRate = 1700;
+        break;
+    
    }
+   netSallary = salary + otHrs * otRate;
+   std::cout<<"Net Salary is "<<setiosflagsstd::(ios::fixed)<<std::setprecision(1)<<netSallary<<endl;
 
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
-  
    return 0;
-}
+ }
